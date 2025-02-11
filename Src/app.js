0af8205df250b1a9ee49f8ba0e45bb21018b67c7
Juanclbici <juan.lopez4572@alumnos.udg.mx>
2025-02-10 20:05:33 -0600

    Funcion suma y register agregada

--- Src/app.js
+++ Src/app.js
@@ -10,10 +10,47 @@ app.use(express.json());
 
 // Se define una ruta GET en la raíz ('/') del servidor.
 // Cuando un usuario accede a esta ruta, se ejecuta la función callback.
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
 
     // Se envía una respuesta al cliente con el texto 'Hello world'.
     res.send('Hello world'); 
+});
+
+app.get('/sum', (req, res) => {
+    const { num1, num2 } = req.query;
+
+    // Convertir los valores a números
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+
+    // Validar si los valores son números
+    if (isNaN(n1) || isNaN(n2)) {
+        return res.status(400).json({ error: "Los parámetros deben ser números válidos" });
+    }
+
+    const resultado = n1 + n2;
+    
+    res.json({ resultado });
+});
+
+
+// Ruta POST para recibir los datos de login
+app.post('/register', (req, res) => {
+    const { email, password } = req.body;
+    
+    // Validar que se envíen los datos necesarios
+    if (!email || !password) {
+        return res.status(400).json({ error: "Se requieren usuario y contraseña" });
+    }
+
+    // Respuesta 
+    res.json({
+        message: "Usuario registrado con éxito",
+        usuario: {
+            email,
+            password
+        }
+    });
 });
 
 // Se exporta la instancia de la aplicación 'app' para que pueda ser utilizada en otros archivos.
